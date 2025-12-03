c621776c3f5d4ba2ac5480f2ab0e389756635154
Rayman <156027290+gunfigterarusil@users.noreply.github.com>
2025-12-03 07:15:02 +0200

    Update KeyBindings.java

--- src/main/java/com/wavedefense/events/KeyBindings.java
+++ src/main/java/com/wavedefense/events/KeyBindings.java
@@ -11,27 +11,28 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.glfw.GLFW;
 
-@Mod.EventBusSubscriber(modid = WaveDefenseMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class KeyBindings {
-
     public static final String CATEGORY = "key.categories.wavedefense";
-
     public static KeyMapping openMenuKey;
 
-    @SubscribeEvent
-    public static void onRegisterKeyMappings(RegisterKeyMappingsEvent event) {
-        openMenuKey = new KeyMapping(
-                "key.wavedefense.openmenu",
-                KeyConflictContext.IN_GAME,
-                InputConstants.Type.KEYSYM,
-                GLFW.GLFW_KEY_V,
-                CATEGORY
-        );
-        event.register(openMenuKey);
+    @Mod.EventBusSubscriber(modid = WaveDefenseMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ModEventHandler {
+        @SubscribeEvent
+        public static void onRegisterKeyMappings(RegisterKeyMappingsEvent event) {
+            openMenuKey = new KeyMapping(
+                    "key.wavedefense.openmenu",
+                    KeyConflictContext.IN_GAME,
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_V,
+                    CATEGORY
+            );
+            event.register(openMenuKey);
+            WaveDefenseMod.LOGGER.info("Key bindings registered");
+        }
     }
 
     @Mod.EventBusSubscriber(modid = WaveDefenseMod.MODID, value = Dist.CLIENT)
-    public static class ClientEvents {
+    public static class ClientForgeEvents {
         @SubscribeEvent
         public static void onClientTick(TickEvent.ClientTickEvent event) {
             if (openMenuKey != null && event.phase == TickEvent.Phase.END) {
