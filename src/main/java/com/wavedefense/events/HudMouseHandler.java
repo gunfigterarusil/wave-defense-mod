fe527b802c5d5a8f598763421fc1d316cce21353
Rayman <156027290+gunfigterarusil@users.noreply.github.com>
2025-12-03 07:16:13 +0200

    Update HudMouseHandler.java

--- src/main/java/com/wavedefense/events/HudMouseHandler.java
+++ src/main/java/com/wavedefense/events/HudMouseHandler.java
@@ -1,8 +1,9 @@
 package com.wavedefense.events;
 
 import com.wavedefense.WaveDefenseMod;
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -10,10 +11,16 @@ import net.minecraftforge.fml.common.Mod;
 public class HudMouseHandler {
 
     @SubscribeEvent
-    public static void onMouseClick(ScreenEvent.MouseButtonPressed.Pre event) {
-        if (event.getButton() == 0) { // Left click
-            if (HudOverlay.handleClick(event.getMouseX(), event.getMouseY())) {
-                event.setCanceled(true);
+    public static void onMouseClick(InputEvent.MouseButton.Pre event) {
+        if (event.getButton() == 0 && event.getAction() == 1) { // Left click pressed
+            Minecraft mc = Minecraft.getInstance();
+            if (mc.screen == null) { // Only when no GUI is open
+                double mouseX = mc.mouseHandler.xpos() * mc.getWindow().getGuiScaledWidth() / mc.getWindow().getScreenWidth();
+                double mouseY = mc.mouseHandler.ypos() * mc.getWindow().getGuiScaledHeight() / mc.getWindow().getScreenHeight();
+                
+                if (HudOverlay.handleClick(mouseX, mouseY)) {
+                    event.setCanceled(true);
+                }
             }
         }
     }
